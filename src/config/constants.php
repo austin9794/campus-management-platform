3822af9039b08586d1a713a9b485436cf18307f8
austin9794 <mohdumair72914@gmail.com>
2026-06-08 22:52:02 +0530

    enrollment

--- src/config/constants.php
+++ src/config/constants.php
@@ -3,3 +3,8 @@
 define('ROLE_ADMIN',   'admin');
 define('ROLE_FACULTY', 'faculty');
 define('ROLE_STUDENT', 'student');
+
+// Enrollment Statuses
+define('ENROLLMENT_ACTIVE',    'active');
+define('ENROLLMENT_DROPPED',   'dropped');
+define('ENROLLMENT_COMPLETED', 'completed');
