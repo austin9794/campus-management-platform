3c5dd59de0f9e7b15481317737afb880b030f8df
austin9794 <mohdumair72914@gmail.com>
2026-06-09 10:15:05 +0530

    Auth class

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -1,1 +1,18 @@
+<?php
+namespace App\Controllers;
 
+use App\Models\User;
+use App\Services\AuthService;
+use App\Helpers\SessionHelper;
+use App\Helpers\ValidationHelper;
+use App\Helpers\ResponseHelper;
+use App\Services\EmailService;
+
+class AuthController {
+    /** GET /login */
+    public static function showLogin(): void {
+        if (SessionHelper::isLoggedIn()) {
+            ResponseHelper::redirect(SessionHelper::dashboardUrl());
+        }
+        require_once ROOT_PATH . '/views/auth/login.php';
+    }
